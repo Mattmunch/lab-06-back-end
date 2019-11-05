bbc5e782f846d21da732e5f9ef4b35a1d4bdd6a7
joelpdurham <joeldur@gmail.com>
2019-11-05 10:34:41 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+
+app.get('/location', (request, response) => {
+    try {
+        const location = request.query.location;
+        const result = getLatLng(location);
+        response.status(200).json(result);
+    }
+    catch (err) {
+        response.status(500).send('Sorry something went wrong, please try again.');
+    }
+});
+
+const geoData = require('./data/geo.json');
+
+function getLatLng(location) {
+    if (location === 'bad location') throw new Error();
+
+    return toLocation(geoData);
+}
+
+function toLocation() {
+    const firstResult = geoData.result[0];
+    const geometry = firstResult.geometry;
+
+    return {
+        formatted_query: firstResult.formatted_address,
+        latitude: geometry.location.lat,
+        longitude: geometry.location.lng
+    };
+}
+
+app.listen(PORT, () => {
+    console.log('server running on PORT', PORT);
+});
