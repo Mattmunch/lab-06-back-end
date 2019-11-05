97aa2b305a758703fda2f14c48d4c2c898ab615b
joelpdurham <joeldur@gmail.com>
2019-11-05 12:00:19 -0800

    got some things to work

--- server.js
+++ server.js
@@ -8,38 +8,73 @@ const PORT = process.env.PORT;
 app.use(cors());
 
 app.get('/location', (request, response) => {
-    console.log('hello');
     try {
-        console.log(request.query);
         const location = request.query.location;
-        console.log(location);
         const result = getLatLng(location);
-        console.log(result);
         response.status(200).json(result);
     }
     catch (err) {
+
         response.status(500).send('Sorry EVERYTHING went wrong, please try again.');
     }
 });
+
 
 const geoData = require('./data/geo.json');
 
 function getLatLng(location) {
     if (location === 'bad location') throw new Error();
-
+    
     return toLocation(geoData);
 }
 
 function toLocation() {
-    const firstResult = geoData.result[0];
+    const firstResult = geoData.results[0];
     const geometry = firstResult.geometry;
-
+    
     return {
         formatted_query: firstResult.formatted_address,
         latitude: geometry.location.lat,
         longitude: geometry.location.lng
     };
 }
+
+
+
+
+app.get('/weather', (request, response) => {
+    try {
+        const location = request.query.location;
+        const result = getWeather(location);
+        response.status(200).json(result);
+    }
+    catch (err) {
+        console.log(err);
+        response.status(500).send('Sorry WEATHER went wrong, please try again.');
+    }
+});
+
+const darkSkyData = require('./data/darksky.json');
+
+function getWeather(location) {
+    if (location === 'bad location') throw new Error();
+    
+    return toWeather(darkSkyData);
+}
+
+function toWeather() {
+    const firstResult = darkSkyData;
+    
+    return {
+        forecast: firstResult.hourly.summary,
+        time: firstResult.hourly.data[0].time
+    };
+}
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log('server running on PORT', PORT);
